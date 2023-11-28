f055e0f9117db5d98286ed8d9e91a8c5df709837
Jose Juarez <Jjuareze2002@gmail.com>
2023-11-27 19:39:15 -0600

    fixing cors error once again

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ const corsOptions = {
     credentials: true, // This is important.
   }
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://truelearn.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser()); 
