d0cd44eca6ce99f981364229a2914bb6408059fd
Jose Juarez <Jjuareze2002@gmail.com>
2023-11-27 19:49:17 -0600

    making few changes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const corsOptions = {
   }
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://truelearn.onrender.com');
+    res.header('Access-Control-Allow-Origin', 'https://truelearn.onrender.com', 'https://d3n6kitjvdjlm1.cloudfront.net/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
